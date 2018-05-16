5daafc5f38d4bee3f50332e7ac3bc8db2eefd624
“linjunjj” <1045735402@qq.com>
2018-05-16 22:41:50 +0800

    添加多个类

--- app/src/main/java/com/linjun/ui/listener/IBase.kt
+++ app/src/main/java/com/linjun/ui/listener/IBase.kt
@@ -1,7 +1,33 @@
 package com.linjun.ui.listener
 
+import android.os.Bundle
+import android.view.View
+import com.linjun.component.ApplicationComponent
+
 interface IBase{
-/*
-* 获取页面布局 ID*/
-    fun getContextLayout（
+    /**
+     * 获取页面布局 id
+     */
+    fun getContentLayout(): Int
+
+    /**
+     * 获取状态布局
+     */
+    fun getSimpleMultiStateView(): SimpleMultiStateView?
+
+    /**
+     * Dagger2 注入
+     */
+    fun initInjector(appComponent: ApplicationComponent)
+
+    /**
+     * 初始化布局
+     */
+    fun bindView(view: View, savedInstanceState: Bundle?)
+
+    /**
+     * 加载数据
+     */
+    fun initData()
+
 }
